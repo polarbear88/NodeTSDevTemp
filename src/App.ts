de63df9e7fe63f291cb9e0d9f317224b7f706eab
小白熊 <mclouds@126.com>
2022-02-26 03:53:04 +0800

    基础配置

---
+++ src/App.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: polarbear
+ * @Date: 2022-02-26 02:33:37
+ * @LastEditTime: 2022-02-26 03:51:33
+ * @LastEditors: polarbear
+ * @Description: 
+ * @FilePath: /NodeTSDevTemp/src/App.ts
+ */
+
+console.log("Hello world");
