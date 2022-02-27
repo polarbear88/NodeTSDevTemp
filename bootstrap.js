4495a957a729a1140ff7ada6044592f78f79ff80
小白熊 <mclouds@126.com>
2022-02-28 01:04:20 +0800

    修改名称

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,10 @@
 /*
  * @Author: polarbear
  * @Date: 2022-02-26 22:14:19
- * @LastEditTime: 2022-02-27 01:42:08
+ * @LastEditTime: 2022-02-28 00:38:12
  * @LastEditors: polarbear
  * @Description: 
- * @FilePath: /NodeTSDevTemp/build.js
+ * @FilePath: /NodeTSDevTemp/bootstrap.js
  */
 require("bytenode");
 // 这样做可以防止pkg将./index-en.jsc拿去编译字节码
